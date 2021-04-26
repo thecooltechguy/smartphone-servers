c4feb6d51d92462c81724a0a260baf7e5a76687a
Ruohan Hu <r8hu@ucsd.edu>
2021-04-26 04:45:37 -0700

    hardcode the params for now, need to change

---
+++ phones/client.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import threading
+
+def register_device(url, id):
+    print("Sending request to register device")
+    # hardcode the params for now, need to query the data using API
+    params = {
+        "id" : id, 
+        "timestamp" : time.ctime()
+    }
+    resp = requests.post(url, json = params)
+    print(resp.status_code)
+
+def send_heartbeat(url):
+    print("Sending heartbeat")
+    # hardcode the params for now, need to query the data using API
+    params = {
+        "timestamp" : time.ctime(),
+        "system" : {
+        "cpu" : 0.8,
+        "memory" : 16
+    }}
+    resp = requests.post(url, json = params)
+    if resp.status_code == 400:
+        # hardcode, need to change
+        register_device("http://0.0.0.0:5000/devices/register/", "dev2")
+        resp = requests.post(url, json = params)
+    # send a heartbeat every 60 seconds
+    threading.Timer(60, send_heartbeat, [url]).start()
+    print(resp.status_code)
+
+
+register_device("http://0.0.0.0:5000/devices/register/", "dev1")
+send_heartbeat("http://0.0.0.0:5000/devices/dev2/heartbeat/")
