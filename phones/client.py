aa954f669468c35c4ffb5e99ccc7c6b3899159d9
Ruohan Hu <r8hu@ucsd.edu>
2021-04-27 00:13:50 -0700

    modify the logic

--- phones/client.py
+++ phones/client.py
@@ -3,18 +3,28 @@ import time
 import threading
 import subprocess
 
-def register_device(url, id):
-    print("Sending request to register device")
+def register_device():
+    url = "http://0.0.0.0:5000/devices/register/"
     # hardcode the params for now
     params = {
-        "id" : id, 
         "timestamp" : time.ctime()
     }
     resp = requests.post(url, json = params)
-    print(resp.status_code)
+    if resp.status_code != 200:
+        print("Unsuccessful Request")
+    
+    id = resp.json()['device_id']
+    # store the device id locally in a file
+    with open("./id.txt", "w") as f:
+        f.write(id)
 
-def send_heartbeat(url):
+def send_heartbeat():
     print("Sending heartbeat")
+    # get the device id
+    id = None
+    with open("./id.txt", "r") as f:
+        id = f.readline()
+    
     '''
     result = subprocess.Popen(["upower"], stdout = subprocess.PIPE)
     output = (result.communicate())
@@ -23,18 +33,20 @@ def send_heartbeat(url):
     params = {
         "timestamp" : time.ctime(),
         "system" : {
-        "cpu" : 0.8,
-        "memory" : 16
-    }}
+            "cpu" : 0.8,
+            "memory" : 16 
+        }
+    }
+    url = "http://0.0.0.0:5000/devices/{}/heartbeat/".format(id)
     resp = requests.post(url, json = params)
     if resp.status_code == 400:
-        # hardcode, need to change
-        register_device("http://0.0.0.0:5000/devices/register/", "dev2")
+        register_device()
         resp = requests.post(url, json = params)
-    # send a heartbeat every 60 seconds
-    threading.Timer(60, send_heartbeat, [url]).start()
+    
     print(resp.status_code)
 
+    # send a heartbeat every 60 seconds
+    threading.Timer(60, send_heartbeat).start()
 
-register_device("http://0.0.0.0:5000/devices/register/", "dev1")
-send_heartbeat("http://0.0.0.0:5000/devices/dev2/heartbeat/")
+register_device()
+send_heartbeat()
