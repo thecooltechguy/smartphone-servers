1d9e2e1bc420621c3b516ac079ecb39bc336fb52
Ruohan Hu <r8hu@ucsd.edu>
2021-05-08 04:27:55 -0700

    add logic to process git tasks

--- phones/client.py
+++ phones/client.py
@@ -1,6 +1,7 @@
 import socketio
 import time
 import requests
+import os
 
 SERVER_ENDPOINT = "http://localhost:5000"
 STATUS_FAILED = 2
@@ -33,14 +34,35 @@ def task_submission(data):
     print(f'Working on job id={job_id}: ')
     print(data['job'])
 
+    # process the task from git repo
+    # TODO: process more complicated tasks
+    process_git_task(data['job']['code_url'])
+
+    # TODO: have more logic in terms of catching and handling errors, and process results
+    result = ""
+    with open("./output", "r") as f:
+        result += f.readline()
+    
     time.sleep(5)
 
     # Once the job succeeds/fails, notify the server
     status = STATUS_SUCCEDED # or, STATUS_FAILED
-    resp = requests.post(f"{SERVER_ENDPOINT}/jobs/{job_id}/update_status/", json={"device_id" : device_id, "status" : status}).json()
+    
+    # TODO: sending the response
+    resp = requests.post(f"{SERVER_ENDPOINT}/jobs/{job_id}/update_status/", json={"device_id" : device_id, "status" : status, "result" : result}).json()
 
-    print (f"Response from notifying server of job status: {status}")
+    print(f"Response from notifying server of job status: {status}")
     print(resp)
+
+def process_git_task(url):
+    # clone the git repo
+    os.system('git clone {}'.format(url))
+    # get the directory
+    directory = url.split('/')[-1].replace('.git', '')
+    # run the file and store in a output file
+    os.system('./{}/main.sh > ./output'.format(directory))
+    # remove the git repo
+    os.system('rm -rf {}'.format(directory))
 
 sio.connect(f'http://127.0.0.1:5000/?device_id={device_id}')
 sio.wait()
