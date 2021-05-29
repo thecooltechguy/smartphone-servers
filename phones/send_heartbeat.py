e1e6cffff51f33940ace0220767a8824b1f81cdd
subby <subby8pie@gmail.com>
2021-05-29 16:36:49 -0700

    Fixed send heartbeat.py file

--- phones/send_heartbeat.py
+++ phones/send_heartbeat.py
@@ -4,13 +4,14 @@ import threading
 import util
 
 HEARTBEAT_INTERVAL_SECS = 5
+SERVER_ENDPOINT = "http://localhost:5000"
 
 # get the device id (should already exist on the phone)
 with open("./id.txt", "r") as f:
     device_id = int(f.readline())
 
 def register_device():
-    url = "http://0.0.0.0:5000/devices/register/"
+    url = f"{SERVER_ENDPOINT}/devices/register/"
     # hardcode the params for now
     params = {
         "timestamp" : time.ctime(),
@@ -42,7 +43,7 @@ def send_heartbeat():
     #        "battery": util.battery_level(),
     #        "disk": util.disk_use(),
     #        "pluggedin": util.plugged_in()
-    url = "http://0.0.0.0:5000/devices/{}/heartbeat/".format(device_id)
+    url = f"{SERVER_ENDPOINT}/devices/{device_id}/heartbeat/"
     resp = requests.post(url, json = params)
     resp_json = resp.json()
     if resp.status_code == 400 and resp_json['error_code'] == "DEVICE_NOT_FOUND":
